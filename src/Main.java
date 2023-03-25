bb69e9433804046a1b5abed93da6271dc32766cb
адина <adina.atakulova04@gmail.om>
2023-03-25 19:36:16 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import javax.xml.namespace.QName;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(aVoid(1, 3, 5, 7, 9, 5, 45));
+//    }0
+
+//    static int aVoid(int... s) {
+//        int summa = 0;
+//        for (int j : s) {
+//            summa = summa + j;
+//        }
+//        return summa;
+//        String[] a = new String[]{"Adina", "Nurbek", "Bayastan"};
+//        names(a);
+//
+//    }
+//
+//    static void names(String[] array) {
+//        for (String a : array) {
+//            System.out.println("мен сени жакшы кором " + a);
+//
+        System.out.println();
+        }
+    }
+
+
+
